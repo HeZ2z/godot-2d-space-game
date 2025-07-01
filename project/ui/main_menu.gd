08c3957f21038b3802c26f30e1b94129185c161d
HeZi <563853317@qq.com>
2025-07-02 04:22:11 +0800

    Add new SVG assets and their import configurations
    - Added dustbak.svg and its import configuration for texture compression.
    - Introduced asteroid-roundbak.svg and its corresponding import settings.
    - Included coinbak.svg with its import configuration for texture processing.
    - Added space-stationbak.svg and its import settings for compressed texture.
    - Introduced iron_orebak.svg and its import configuration for texture compression.

--- project/ui/main_menu.gd
+++ project/ui/main_menu.gd
@@ -5,7 +5,6 @@ const FADE_IN_TIME := 0.5  # 淡入时间
 const FADE_OUT_TIME := 2.5  # 淡出时间
 
 @onready var screen_fader: TextureRect = $FadeLayer/ScreenFader  # 屏幕淡入淡出控制器
-@onready var main_screen := $Background/MainScreen  # 主屏幕
 
 @onready var menu_sounds: MenuSoundPlayer = $MenuSoundPlayer  # 菜单音效播放器
 
@@ -19,7 +18,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 	if event is InputEventKey:
 		# 如果是键盘事件，开始切换场景
-		main_screen.animator.play_backwards("intro")
 		menu_sounds.play_confirm()
 		screen_fader.fade_out()
 		await screen_fader.animation_finished
